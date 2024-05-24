563f6902a66e7aa67453e951286e60057e483493
Mateo <matuserafini@gmail.com>
2024-05-23 21:04:57 -0300

    mangers sin comentarios

--- src/dao/controllers/Mongo/messageManagerMongo.js
+++ src/dao/controllers/Mongo/messageManagerMongo.js
@@ -1,53 +1,35 @@
-// Importo el modelo de mensajes desde el archivo correspondiente
 import messageModel from "../../models/messages.model.js";
 
-// Defino la clase MessageManager para manejar operaciones relacionadas con mensajes
 export default class MessageManager {
-  // Método para obtener todos los mensajes
   getMessages = async () => {
     try {
-      // Retorno todos los mensajes en formato lean (sin métodos de Mongoose)
       return await messageModel.find().lean();
     } catch (error) {
-      // Registro un error en la consola si ocurre durante la obtención de mensajes
       console.log("Error al obtener mensajes", error);
-      // Lanzo el error para ser manejado por quien llame a la función
       throw error;
     }
   };
 
-  // Método para crear un nuevo mensaje
   createMessage = async (message) => {
-    // Verifico que el usuario y el mensaje no estén vacíos
     if (message.user.trim() === "" || message.message.trim() === "") {
       return null;
     }
     try {
-      // Creo un nuevo mensaje en la base de datos
       return await messageModel.create(message);
     } catch (error) {
-      // Registro un error en la consola si ocurre durante la creación del mensaje
       console.log("Error al guardar mensaje", error);
-      // Lanzo el error para ser manejado por quien llame a la función
       throw error;
     }
   };
 
-  // Método para borrar todos los mensajes
   deleteAllMessages = async () => {
     try {
-      // Registro un mensaje en la consola indicando que estoy borrando los mensajes
       console.log("Borrando mensajes...");
-      // Borro todos los mensajes de la base de datos
       const result = await messageModel.deleteMany({});
-      // Registro el resultado en la consola
       console.log("Mensajes borrados:", result);
-      // Retorno el resultado
       return result;
     } catch (error) {
-      // Registro un error en la consola si ocurre durante el borrado de mensajes
       console.error("Error al borrar mensajes:", error);
-      // Retorno el error
       return error;
     }
   };
--- src/dao/controllers/Mongo/productManagerMongo.js
+++ src/dao/controllers/Mongo/productManagerMongo.js
@@ -1,25 +1,19 @@
-// Importo el modelo de productos desde el archivo correspondiente
 import { productsModel } from "../../models/products.model.js";
 
-// Defino la clase ProductManager para manejar operaciones relacionadas con productos
 export default class ProductManager {
-  // Método para obtener todos los productos
   getProducts = async (queryParams) => {
     try {
       const { limit = 10, page = 1, sort, query } = queryParams;
       const skip = (page - 1) * limit;
 
-      // Construyo la consulta
       let filter = {};
       if (query) {
-        // Permito filtrar por categoría o disponibilidad
         filter = {
           ...filter,
           $or: [{ category: query }, { availability: query }],
         };
       }
 
-      // Configuro el sort si se proporciona
       let sortOption = {};
       if (sort === "asc") {
         sortOption = { price: 1 };
@@ -27,7 +21,6 @@ export default class ProductManager {
         sortOption = { price: -1 };
       }
 
-      // Obtengo productos de la base de datos según los filtros, limit y sort especificados
       const products = await productsModel
         .find(filter)
         .sort(sortOption)
@@ -36,13 +29,11 @@ export default class ProductManager {
       const totalProducts = await productsModel.countDocuments(filter);
       const totalPages = Math.ceil(totalProducts / limit);
 
-      // Calculo las páginas previas y siguientes
       const hasPrevPage = page > 1;
       const hasNextPage = page < totalPages;
       const prevPage = hasPrevPage ? page - 1 : null;
       const nextPage = hasNextPage ? page + 1 : null;
 
-      // Retorno un objeto con los datos requeridos
       return {
         status: "success",
         payload: products,
@@ -61,28 +52,21 @@ export default class ProductManager {
       };
     } catch (error) {
       console.log("Error al obtener productos", error);
-      // Lanza el error para ser manejado por quien llame a la función
       throw error;
     }
   };
 
-  // Método para obtener todos los productos en formato lean (sin métodos de Mongoose)
   getProductsView = async () => {
     try {
-      // Retorno todos los productos en formato lean
       return await productsModel.find().lean();
     } catch (error) {
-      // Registro un error en la consola si ocurre durante la obtención de productos
       console.log("Error al obtener productos en formato lean", error);
-      // Lanzo el error para ser manejado por quien llame a la función
       throw error;
     }
   };
 
-  // Método para obtener un producto por ID
   async getProductById(id) {
     try {
-      // Busco el producto por ID en la base de datos
       const producto = await productsModel.findById(id);
       if (!producto) {
         console.log("Producto no encontrado");
@@ -91,36 +75,27 @@ export default class ProductManager {
       console.log("Producto encontrado");
       return producto;
     } catch (error) {
-      // Registro un error en la consola si ocurre durante la búsqueda del producto por ID
       console.log("Error al encontrar producto por ID", error);
-      // Lanzo el error para ser manejado por quien llame a la función
       throw error;
     }
   }
 
-  // Método para agregar un nuevo producto
   addProduct = async (product) => {
     try {
-      // Creo un nuevo producto en la base de datos
       await productsModel.create(product);
-      // Retorno el producto recién creado
       return await productsModel.findOne({ title: product.title });
     } catch (error) {
-      // Registro un error en la consola si ocurre durante la creación del producto
       console.log("Error al crear un producto", error);
-      // Lanzo el error para ser manejado por quien llame a la función
       throw error;
     }
   };
 
-  // Método para actualizar un producto por ID
   async updateProduct(id, productoActualizado) {
     try {
-      // Actualizo el producto por ID en la base de datos
       const updateProduct = await productsModel.findByIdAndUpdate(
         id,
         productoActualizado,
-        { new: true } // Para retornar el producto actualizado
+        { new: true } 
       );
       if (!updateProduct) {
         console.log("Producto no encontrado");
@@ -129,17 +104,13 @@ export default class ProductManager {
       console.log("Producto actualizado");
       return updateProduct;
     } catch (error) {
-      // Registro un error en la consola si ocurre durante la actualización del producto por ID
       console.log("Error al actualizar producto por ID", error);
-      // Lanzo el error para ser manejado por quien llame a la función
       throw error;
     }
   }
 
-  // Método para eliminar un producto por ID
   async deleteProduct(id) {
     try {
-      // Elimino el producto por ID de la base de datos
       const deleteProduct = await productsModel.findByIdAndDelete(id);
 
       if (!deleteProduct) {
@@ -148,9 +119,7 @@ export default class ProductManager {
       }
       console.log("Producto eliminado");
     } catch (error) {
-      // Registro un error en la consola si ocurre durante la eliminación del producto por ID
       console.log("Error al eliminar producto por ID", error);
-      // Lanzo el error para ser manejado por quien llame a la función
       throw error;
     }
   }
