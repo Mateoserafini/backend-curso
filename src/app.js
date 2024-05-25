2c05f5e12cd7242ac1dd9af6e4d872c3a5b524e3
Mateo <matuserafini@gmail.com>
2024-05-24 21:31:58 -0300

    comentarios terminados

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
-// Importa las dependencias necesarias
-import express from "express"; // Importa la biblioteca Express para crear el servidor web
-import { __dirname } from "./utils.js"; // Importa la función para obtener el directorio actual
-import handlebars from "express-handlebars"; // Importa el motor de plantillas Handlebars
-import { Server } from "socket.io"; // Importa la biblioteca para trabajar con WebSocket
-import "./database.js"; // Importa la configuración de la base de datos
-import "dotenv/config"; // Importa las variables de entorno desde el archivo .env
-import routerProduct from "./routes/products.router.js"; // Importa las rutas de productos
-import routerViews from "./routes/views.router.js"; // Importa las rutas para las vistas
-import cartRouter from "./routes/cart.router.js"; // Importa las rutas para carritos
-import socketProducts from "./listeners/socketProducts.js"; // Importa el controlador de productos para WebSocket
-import socketChat from "./listeners/socketChat.js"; // Importa el controlador de chat para WebSocket
+import express from "express";
+import { __dirname } from "./utils.js";
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+import "./database.js";
+import "dotenv/config";
+import routerProduct from "./routes/products.router.js";
+import routerViews from "./routes/views.router.js";
+import cartRouter from "./routes/cart.router.js";
+import socketProducts from "./listeners/socketProducts.js";
+import socketChat from "./listeners/socketChat.js";
 import userRouter from "./routes/user.router.js";
 import sessionsRouter from "./routes/session.router.js";
 import session from "express-session";
@@ -17,21 +16,16 @@ import MongoStore from "connect-mongo";
 import passport from "passport";
 import initializePassport from "./config/passport.config.js";
 
-// Crea una instancia de la aplicación Express
 const app = express();
-
-// Obtiene el puerto desde las variables de entorno
 const PORT = process.env.PORT;
 
-// Configura middleware para manejar JSON y archivos estáticos
-app.use(express.json()); // Configura el manejo de datos en formato JSON
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public")); // Configura el acceso a archivos estáticos en la carpeta "public"
+app.use(express.static(__dirname + "/public"));
 
-// Configura el motor de plantillas Handlebars
 app.engine("handlebars", handlebars.engine());
-app.set("views", __dirname + "/views"); // Establece el directorio de las vistas
-app.set("view engine", "handlebars"); // Establece Handlebars como motor de plantillas
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
 
 app.use(
   session({
@@ -39,8 +33,7 @@ app.use(
     resave: true,
     saveUninitialized: true,
     store: MongoStore.create({
-      mongoUrl:
-        "mongodb+srv://matuserafini:45089673@cluster0.frnygq1.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
+      mongoUrl: "mongodb+srv://matuserafini:45089673@cluster0.frnygq1.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
       ttl: 15,
     }),
   })
@@ -50,21 +43,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 initializePassport();
 
-// Configura las rutas de la aplicación
 app.use("/api/users", userRouter);
 app.use("/api/sessions", sessionsRouter);
-app.use("/api/products", routerProduct); // Establece las rutas para productos
-app.use("/api/carts", cartRouter); // Establece las rutas para carritos
-app.use("/", routerViews); // Establece las rutas para las vistas
+app.use("/api/products", routerProduct);
+app.use("/api/carts", cartRouter);
+app.use("/", routerViews);
 
-// Inicia el servidor HTTP y escucha en el puerto especificado
 const httpServer = app.listen(PORT, () => {
-  console.log(`Escuchando en http://localhost:${PORT}`); // Muestra un mensaje indicando que el servidor está escuchando
+  console.log(`Escuchando en http://localhost:${PORT}`);
 });
 
-// Crea un servidor WebSocket utilizando el servidor HTTP
 const socketServer = new Server(httpServer);
 
-// Inicializa los controladores de WebSocket
-socketProducts(socketServer); // Inicializa el controlador de productos
-socketChat(socketServer); // Inicializa el controlador de chat
+socketProducts(socketServer);
+socketChat(socketServer);
