6884e30a7e33cb9efc6cc21c4071dbc2a4233b75
Mateo <matuserafini@gmail.com>
2024-07-16 11:49:07 -0300

    modified auth and routes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,8 +1,14 @@
-export function authorize(role) {
-    return (req, res, next) => {
-      if (!req.user || req.user.role !== role) {
-        return res.status(403).send("Acceso denegado");
-      }
-      next();
-    };
+export function authorize(roles) {
+  if (typeof roles === "string") {
+    roles = [roles];
   }
+
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .send("Acceso denegado: No tienes los permisos necesarios.");
+    }
+    next();
+  };
+}
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,27 +1,33 @@
 import { Router } from "express";
-import ViewsController from "../controllers/views.controller.js"
+import ViewsController from "../controllers/views.controller.js";
 import { authorize } from "../middleware/auth.js";
 import generarProductos from "../mocking.js";
 
-const viewsController = new ViewsController()
-
+const viewsController = new ViewsController();
 const router = Router();
 
 router.get("/", viewsController.home);
-router.get("/realtimeproducts", authorize("admin"), viewsController.realtimeproducts);
-router.get("/chat", authorize("user"), viewsController.chat);
-router.get("/products", authorize("user"), viewsController.products);
+router.get(
+  "/realtimeproducts",
+  authorize(["admin", "premium"]),
+  viewsController.realtimeproducts
+);
+router.get("/chat", authorize(["user", "premium"]), viewsController.chat);
+router.get(
+  "/products",
+  authorize(["user", "premium"]),
+  viewsController.products
+);
 router.get("/carts/:cid", viewsController.cart);
 router.get("/login", viewsController.login);
 router.get("/register", viewsController.register);
 router.get("/profile", viewsController.profile);
 router.get("/mocking", (req, res) => {
-    const productsMocking = [];
-    for (let i = 0; i < 100
-        ; i++ ){
-        productsMocking.push(generarProductos());
-    }
-    res.send(productsMocking)
-})
+  const productsMocking = [];
+  for (let i = 0; i < 100; i++) {
+    productsMocking.push(generarProductos());
+  }
+  res.send(productsMocking);
+});
 
 export default router;
