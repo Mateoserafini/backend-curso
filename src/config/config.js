540b05559ff6e6572ed8495f62884ba9b984ba4d
Mateo <matuserafini@gmail.com>
2024-08-28 14:52:05 -0300

    bugcorrection en .envs

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,35 @@
 import dotenv from "dotenv";
+import fs from "fs";
+import program from "../utils.js";
 
-import program from "../utils.js"; 
+const { mode } = program.opts();
 
-const { mode } = program.opts(); 
+const envPaths = {
+    produccion: "./.env.produccion",
+    desarrollo: "./.env.desarrollo",
+    default: "./.env",
+};
 
-dotenv.config({
-    path: mode ==="produccion" ? "./.env.produccion" : "./.env.desarrollo"
-});
+let envPath = envPaths[mode] || envPaths.default;
+
+if (!fs.existsSync(envPath)) {
+    envPath = envPaths.default;
+}
+
+dotenv.config({ path: envPath });
 
 const configObject = {
     PORT: process.env.PORT,
     mongoURL: process.env.MONGOURL,
-    clientID:process.env.CLIENTID,
-    clientSecret:process.env.CLIENTSECRET,
-    callbackURL:process.env.CALLBACKURL,
-    secret : process.env.SECRETMONGO,
+    clientID: process.env.CLIENTID,
+    clientSecret: process.env.CLIENTSECRET,
+    callbackURL: process.env.CALLBACKURL,
+    secret: process.env.SECRETMONGO,
     admin01: process.env.ADMIN01,
-    admin02:  process.env.ADMIN02,
-    estado : process.env.estado,
-    mailerUser:process.env.MAILERUSER,
-    mailerPassword:process.env.MAILERPASSWORD,
+    admin02: process.env.ADMIN02,
+    estado: process.env.estado,
+    mailerUser: process.env.MAILERUSER,
+    mailerPassword: process.env.MAILERPASSWORD,
 };
 
 export default configObject;
